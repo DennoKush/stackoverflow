6569e1e6af2a9f6bfe6a37d17c8bb53d0cd9c675
Dennis <dwanjiku@mfs.co.ke>
2019-09-06 13:53:34 +0300

    Some changes to Salary_ML.py file

--- Salary_ML.py
+++ Salary_ML.py
@@ -109,7 +109,7 @@ def main():
     df = pd.read_csv('../Part1/stackoverflow/survey_results_public.csv')
     X, y = clean_data(df)
     #cutoffs here pertains to the number of missing values allowed in the used columns.
-    #Therefore, lower values for the cutoff provides more predictors in the model.
+    #Lower values for the cutoff provides more predictors in the model.
     cutoffs = [5000, 3500, 2500, 1000, 100, 50, 30, 20, 10, 5]
 
     r2_scores_test, r2_scores_train, lm_model, X_train, X_test, y_train, y_test = find_optimal_lm_mod(X, y, cutoffs, plot=False)
